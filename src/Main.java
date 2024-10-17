bdc94d6e832549030a9c8975fd239d0d378e950f
M3m0ry915 <107813607+M3m0ry915@users.noreply.github.com>
2024-10-17 19:08:34 +0200

    feat: adding class rejestracja and code in main, fix title

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Rejestracja");
 
         Scanner scanner = new Scanner(System.in);
 
