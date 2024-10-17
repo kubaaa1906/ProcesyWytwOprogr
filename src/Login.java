c50b7c94c4a1194364a7e89a17f00d81d362c974
Kuba <kubaa0105@gmail.com>
2024-10-17 19:05:28 +0200

    feat: adding class Login and something in main

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+public class Login {
+    public String username;
+    public String password;
+
+    public Login() {}
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz login: ");
+        String login = scanner.nextLine();
+
+        System.out.println("Wprowadz haslo: ");
+        String haslo = scanner.nextLine();
+
+        System.out.println("Twoj login: $" + login);
+        System.out.println("Twoje haslo: $" + haslo);
     }
 }
