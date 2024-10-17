979cdc0ff5475ddaf0320f034f514e0b68fed4cf
Kuba <kubaa0105@gmail.com>
2024-10-17 19:24:31 +0200

    feat: adding class Login with getter and setter AND something in main - fixing v1.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -14,13 +11,13 @@ public class Main {
         System.out.println("Wprowadz haslo: ");
         String haslo = scanner.nextLine();
 
-        System.out.println("Twoj login: $" + login);
-        System.out.println("Twoje haslo: $" + haslo);
+        System.out.println("Twoj login: " + login);
+        System.out.println("Twoje haslo: " + haslo);
 
         System.out.println("FIXED!!!!");
         System.out.println("testesttestest");
 
         Login login1 = new Login(login, haslo);
-        System.out.println(login1);
+
     }
 }
